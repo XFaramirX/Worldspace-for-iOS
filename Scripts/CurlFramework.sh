644bbd3ef2f7550a39a744059df79828d6480618
Chris McMeeking <chris.mcmeeking@deque.com>
2018-02-08 16:55:37 -0500

    feat: Cleaner build script/curl logic. Better curl framework logic and error handling.

--- Scripts/CurlFramework.sh
+++ Scripts/CurlFramework.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
 
-#If it already exists, leave it alone
-if [ -d "./Framework/Release/Attest.framework" ]; then
-    exit 0
+# The version of Xcode you want to fetch the framework for.
+# If you need a given version built for you let us know.
+# Our CI Servers build for XCode 9.# by default.
+XCODE_VERSION=$(xcodebuild -version | head -n 1 | tr -d '[:space:]')
+
+# Configurable VIA Command line
+while getopts "v:x:" opt; do
+    case "$opt" in
+    v)  ATTEST_FRAMEWORK_VERSION=$OPTARG
+        ;;
+    esac
+done
+
+if [ -z "$ATTEST_FRAMEWORK_VERSION" ]; then 
+    echo "Must request a specific version of the Attest Library. (Current: -v 1.2.0)"
+    exit 1
 fi
-
-#Change this to the version of the framework you want.
-FRAMEWORK_VERSION=1.1.2
-
-#The version of Xcode you want to fetch the framework for (Xcode9.0 or Xcode9.1)
-#The Xcode9.0 framework build should work for 8.0 - 9.0.1. If you need another build post an issue.
-XCODE_VERSION=Xcode9.1
 
 #This directory has to match up with your Framework Search paths in XCode Build Configuration.
 FRAMEWORK_DIR=./Framework/Release
-
-#Change version number if specified
-if [ "$1" == "-v" ]; then
-    shift
-    FRAMEWORK_VERSION=$1
-fi
 
 #Ensure it exists and change to it.
 mkdir -p $FRAMEWORK_DIR
@@ -29,7 +29,7 @@ source ~/.bash_profile
 
 #Fetch the library zip with our anonymous api key.
 curl -H "X-JFrog-Art-Api: ${DEQUE_ANON_APIKEY}" \
-	-O "https://agora.dequecloud.com/artifactory/AttestIOSFree/framework/$XCODE_VERSION/Attest.framework-$FRAMEWORK_VERSION.zip"
+	-O "https://agora.dequecloud.com/artifactory/AttestIOSFree/framework/$XCODE_VERSION/Attest.framework-$ATTEST_FRAMEWORK_VERSION.zip"
 
 #Unzip whatever version was fetched
 unzip Attest.*zip
